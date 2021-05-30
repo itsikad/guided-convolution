2405cf2822a4116f7fdce6bd93b50bedc4f1625b
itsikad <41125160+itsikad@users.noreply.github.com>
2021-05-30 18:54:43 +0300

    Update __init__.py
    fixed bug in __init__

--- guided_conv/__init__.py
+++ guided_conv/__init__.py
@@ -1,4 +1,4 @@
-from guided_conv import GuidedChannelWiseConv2d, GuidedDepthWiseConv2d, \
+from .guided_conv import GuidedChannelWiseConv2d, GuidedDepthWiseConv2d, \
     SeparableGuidedConv2d, GuidedLocalConv2d, GuidedConv2d
 
 __all__ = ['GuidedChannelWiseConv2d', 'GuidedDepthWiseConv2d',
